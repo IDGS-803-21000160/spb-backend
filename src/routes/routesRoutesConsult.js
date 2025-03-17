4c44295e74a84575ac0a266a101f928c808eec72
Ivan Blancarte <iblancarte583@gmail.com>
2025-03-17 10:58:56 -0600

    feat: nuevos servicios de consulta

--- src/routes/routesRoutesConsult.js
+++ src/routes/routesRoutesConsult.js
@@ -2,6 +2,14 @@ const express = require("express");
 const router = express.Router();
 const routesRoutes = require("../controllers/controllerRoutes/routesController");
 
-router.get("/:date/:userId", routesRoutes.getAllRoutes);
+router.get(
+  "/operador/:idOperador/:fecha",
+  routesRoutes.getRouteAssignedToOperator
+);
+router.get("/routes/:idOperador/:fecha", routesRoutes.getRouteAndCrData);
+router.get("/rutes/:date/:userId", routesRoutes.getAllRoutes);
+router.get("/cr/:idCr", routesRoutes.getDataFromTheCrThatAssignedRoute);
+
+router.get("/:idRuta/:fecha", routesRoutes.getRouteInfo);
 
 module.exports = router;
